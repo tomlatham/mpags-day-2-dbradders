57ca55dd729ea30c8c7f009bdd9f180593983ee3
David Bradley <d.bradley.1@warwick.ac.uk>
2019-10-18 15:31:50 +0100

    Create new file TransformChar.cpp containing function and include in code

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,56 +6,8 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
-std::string transformChar (const char inputChar) { 
-    // Uppercase alphabetic characters
-  std::string inputText;
-    if (std::isalpha(inputChar)) {
-      inputText += std::toupper(inputChar);
-    }
-
-    // Transliterate digits to English words
-    else{
-      switch (inputChar) {
-      case '0':
-	inputText += "ZERO";
-	break;
-      case '1':
-	inputText += "ONE";
-	break;
-      case '2':
-	inputText += "TWO";
-	break;
-      case '3':
-	inputText += "THREE";
-	break;
-      case '4':
-	inputText += "FOUR";
-	break;
-      case '5':
-	inputText += "FIVE";
-	break;
-      case '6':
-	inputText += "SIX";
-	break;
-      case '7':
-	inputText += "SEVEN";
-	break;
-      case '8':
-	inputText += "EIGHT";
-	break;
-      case '9':
-	inputText += "NINE";
-	break;
-      }};
-
-    // If the character isn't alphabetic or numeric, DONT add it.
-    // Our ciphers can only operate on alphabetic characters.
-
-    return inputText;
-
-}
-
-
+// Transliterate char to string
+std::string TransformChar(const char inputChar);
 
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
@@ -161,7 +113,7 @@ int main(int argc, char* argv[])
   // (until Return then CTRL-D (EOF) pressed)
   while(std::cin >> inputChar)
     {
-      inputText = transformChar(inputChar);
+      inputText = TransformChar(inputChar);
     }
 
   // Output the transliterated text
