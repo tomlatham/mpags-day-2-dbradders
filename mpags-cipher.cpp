39aa431f3128b4f021f8e3a44f04633c2404e26c
David Bradley <d.bradley.1@warwick.ac.uk>
2019-10-25 14:25:07 +0100

    Exercise - Start Using Functions: Completed and corrected

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,7 +6,54 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
-#include "TransformChar.hpp"
+std::string transformChar (const char inputChar){
+      // Uppercase alphabetic characters
+  std::string inputText;
+    if (std::isalpha(inputChar)) {
+      inputText += std::toupper(inputChar);
+      }
+
+    // Transliterate digits to English words
+    else{
+    switch (inputChar) {
+      case '0':
+	inputText += "ZERO";
+	break;
+      case '1':
+	inputText += "ONE";
+	break;
+      case '2':
+	inputText += "TWO";
+	break;
+      case '3':
+	inputText += "THREE";
+	break;
+      case '4':
+	inputText += "FOUR";
+	break;
+      case '5':
+	inputText += "FIVE";
+	break;
+      case '6':
+	inputText += "SIX";
+	break;
+      case '7':
+	inputText += "SEVEN";
+	break;
+      case '8':
+	inputText += "EIGHT";
+	break;
+      case '9':
+	inputText += "NINE";
+	break;
+    }}
+
+    // If the character isn't alphabetic or numeric, DONT add it.
+    // Our ciphers can only operate on alphabetic characters.
+
+  return inputText;
+
+}
 
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
@@ -111,9 +158,10 @@ int main(int argc, char* argv[])
   // Loop over each character from user input
   // (until Return then CTRL-D (EOF) pressed)
   while(std::cin >> inputChar)
-    {
-      inputText = TransformChar(inputChar);
-    }
+  {
+    inputText += transformChar(inputChar);
+  }
+
 
   // Output the transliterated text
   // Warn that output file option not yet implemented
@@ -123,7 +171,7 @@ int main(int argc, char* argv[])
               << "') not implemented yet, using stdout\n";
   }
 
-  std::cout << inputText << std::endl;
+  std::cout << "\n" << inputText << std::endl;
 
   // No requirement to return from main, but we do so for clarity
   // and for consistency with other functions
